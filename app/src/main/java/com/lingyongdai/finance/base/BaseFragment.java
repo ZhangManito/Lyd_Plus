26b6de8329cd541991551855f9ba6e9fa3f7be6d
张国梁 <zhangguoliang@impcredit.com>
2018-02-26 17:34:09 +0800

    MVVM初步模型

---
+++ app/src/main/java/com/lingyongdai/finance/base/BaseFragment.java
@@ -0,0 +1,8 @@
+package com.lingyongdai.finance.base;
+
+/**
+ * Created by guoliang on 2018/2/26.
+ */
+
+public class BaseFragment {
+}
---
+++ app/src/main/java/com/lingyongdai/finance/viewmodel/MActivityViewModel.java
@@ -0,0 +1,8 @@
+package com.lingyongdai.finance.viewmodel;
+
+/**
+ * Created by guoliang on 2018/2/26.
+ */
+
+public class MActivityViewModel {
+}
